587c079447032cc858a843503ce0e830c1b22768
Marcin Szewczyk <ms@anixe.pl>
2012-05-24 10:29:48 +0200

    Add the command 'send_user' for the expect script

--- mac/scripts/expect_ssh.sh
+++ mac/scripts/expect_ssh.sh
@@ -12,6 +12,15 @@ spawn ssh $user@$serv
 expect "password:" { send "$pass\r" }
 send "$comm\r"
 
+# display to the user infos
+send_user "\n"
+# display only the expected word in the case "password:"
+send_user "expect_out(0,string): $expect_out(0,string)\n"
+# display whole text which appeared before the expected word "password:"
+send_user "expect_out(buffer): $expect_out(buffer)\n"
+# sleep for 10 seconds
+sleep 10
+
 # switch off below two lines if you want use interact mode
 send "exit\r"
 expect eof
