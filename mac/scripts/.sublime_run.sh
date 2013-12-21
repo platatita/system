bc4b26c712c570b590c0d191f7d504a4ae2b88d2
platatita <platatita@gmail.com>
2013-12-21 20:46:00 +0100

    [Change] Path to the Sublime editor

--- mac/scripts/.sublime_run.sh
+++ mac/scripts/.sublime_run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # copy the script into home directory because it is invoked by the alias 'alias sublime='~/.sublime_run.sh' in the .bash_aliases file.
-SUBLIMEPATH=/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl
+SUBLIMEPATH=/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl
 
 function sublime(){ 
   local tmp_pwd="$(pwd)"
