f6331a421374cc94f3ed5856e24231b3d0e501f0
marcins <platatita#gmail.com>
2015-04-06 17:31:30 +0200

    Extract anixe folder creation to separate file

--- mac/scripts/directory_creator.sh
+++ mac/scripts/directory_creator.sh
@@ -1,24 +1,22 @@
 #! /bin/bash
 
-echo make Downloads folders
+if [ -f ./directory_creator_anixe.sh ]; then
+  ./directory_creator_anixe.sh
+fi
+
+echo create Downloads folders
 mkdir -pv ~/Downloads/safari
 mkdir -pv ~/Downloads/firefox
 mkdir -pv ~/Downloads/mailbox
 mkdir -pv ~/Downloads/utorrent
 
-echo make Documents folders
+echo create Documents folders
 mkdir -pv ~/Documents/internet
 mkdir -pv ~/Documents/games
 mkdir -pv ~/Documents/developer/os_hints
 mkdir -pv ~/Documents/developer/ios
 mkdir -pv ~/Documents/screencapture
 
-echo make sources folder
+echo create sources folder
 mkdir -pv ~/sources/git
 mkdir -pv ~/tmp
-
-echo make anixe folder structure
-mkdir -pv ~/anx/var/log
-mkdir -pv ~/anx/bin/tools
-mkdir -pv ~/anx/sources/git
-mkdir -pv ~/anx/tmp
---
+++ mac/scripts/directory_creator_anixe.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "create anixe folder structure"
+
+mkdir -pv ~/anx/var/log
+mkdir -pv ~/anx/bin/tools
+mkdir -pv ~/anx/sources/git
+mkdir -pv ~/anx/tmp
+mkdir -pv ~/anx/tmp/exports
+mkdir -pv ~/anx/tmp/resfinity_pa
