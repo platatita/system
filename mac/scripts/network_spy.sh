4bd0a24158633c525d21e72b356daecdcea33e04
Marcin Szewczyk <ms@anixe.pl>
2020-11-29 17:28:26 +0100

    Add comments to the network_spy.sh script

--- mac/scripts/network_spy.sh
+++ mac/scripts/network_spy.sh
@@ -5,8 +5,14 @@ if [ -n "${1}" ]; then
   BROADCASTMASK=$1
 fi
 
-echo IP address to test: $BROADCASTMASK
-
+echo "-------- ping: IP address to test: $BROADCASTMASK --------"
+# ping -- send ICMP ECHO_REQUEST packets to network hosts
+# 	-c: 	count, tries 3 times
 ping -c 3 $BROADCASTMASK
+
+
+echo "-------- arp --------"
+# arp -- address resolution display and control
+# 	-a: 	The program displays or deletes all of the current ARP entries.
 arp -a
 
